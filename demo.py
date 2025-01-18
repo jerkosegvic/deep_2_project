1afe0084095f1f7fd65ecbabe71126775e663e14
antoniolukic <al53056@fer.hr>
2025-01-18 11:39:16 +0100

    Updated demo

--- demo.py
+++ demo.py
@@ -1,7 +1,26 @@
 import gradio as gr
 from PIL import Image, ImageDraw
+import torch
 import numpy as np
 import cv2
+from model import Diffusion
+from repaint_loop import repaint_inpaint
+import os
+
+def load_model():
+    model = Diffusion(image_size=28, in_channels=1, timesteps=1000, base_dim=64).to(device)
+
+    checkpoint_path = "checkpoints/epoch_99.pt"
+    if os.path.exists(checkpoint_path):
+        checkpoint = torch.load(checkpoint_path, map_location=device)
+        model.load_state_dict(checkpoint["model"])
+        model.eval()
+        print("Checkpoint loaded successfully.")
+    else:
+        print(f"Checkpoint not found at {checkpoint_path}. Please run training first.")
+        return
+
+    return model
 
 
 def upsample_image(image_np, scale_factor=10):
@@ -18,7 +37,7 @@ def upsample_image(image_np, scale_factor=10):
     return upsampled_image
 
 
-def downsample_mask(mask_np, target_size=(32, 32)):
+def downsample_mask(mask_np, target_size=(28, 28)):
     """
     Downsample the mask to match the size of the input image (32x32).
     Ensure the mask is a NumPy array before resizing.
@@ -33,40 +52,67 @@ def downsample_mask(mask_np, target_size=(32, 32)):
     return downsampled_mask
 
 
-def inpaint_image(input_image_np, mask_np):
+def inpaint_image(input_image_np, mask_np, model, num_timesteps, U):
     """
     Inpainting function that takes RGB NumPy arrays as input.
     Applies the mask to black out the selected regions.
     """
-    if mask_np.ndim == 2:
-        mask_np = np.expand_dims(mask_np, axis=-1)
-    mask_np = np.repeat(mask_np, 3, axis=-1)
 
-    inpainted_image = np.where(mask_np == 255, 0, input_image_np)
-    return inpainted_image
+    mask_np = np.expand_dims(mask_np, axis=-1)
+    mask_np = np.repeat(mask_np, 3, axis=-1)  # shapes (H, W, 3)
+
+    input_image_gray = cv2.cvtColor(input_image_np, cv2.COLOR_RGB2GRAY)
+    input_image_gray = np.expand_dims(input_image_gray, axis=-1)
+
+    input_image_tensor = torch.tensor(input_image_gray.transpose(2, 0, 1)).unsqueeze(0).float().to(device) / 255.0
+    mask_tensor = torch.tensor(mask_np[:, :, 0]).unsqueeze(0).unsqueeze(0).float().to(device) / 255.0  # shapes (1, 1, H, W)
+
+    masked_image_tensor = input_image_tensor.clone()
+    masked_image_tensor[mask_tensor == 1] = 0
+
+    #print(masked_image_tensor.shape, mask_tensor.shape)
+
+    print("started doing diffusion")
+    inpainted_image_tensor = repaint_inpaint(
+        model=model,
+        input_image=masked_image_tensor,
+        mask=mask_tensor,
+        num_timesteps=num_timesteps,
+        U=U,
+        device=device
+    )
+    print("done with diffusion")
+    # convert the output back to a NumPy array
+    inpainted_image_np = (inpainted_image_tensor.squeeze(0).cpu().detach().numpy().transpose(1, 2, 0) * 255).astype(np.uint8)
+    print("Output shape:")
+    print(inpainted_image_np.shape)
+
+    return inpainted_image_np
 
 
-cifar_images = [
-    np.array(Image.open("img_1.png").resize((32, 32)).convert("RGB")),
-    np.array(Image.open("img_2.png").resize((32, 32)).convert("RGB")),
-    np.array(Image.open("img_4.png").resize((32, 32)).convert("RGB")),
+mnist_images = [
+    np.array(Image.open("img_1.png").resize((28, 28)).convert("RGB")),
+    np.array(Image.open("img_2.png").resize((28, 28)).convert("RGB")),
+    np.array(Image.open("img_3.png").resize((28, 28)).convert("RGB")),
 ]
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = load_model()
 
 
 def demo():
     with gr.Blocks() as demo_interface:
-        gr.Markdown("# CIFAR Inpainting Demo with Sketchpad Masking")
+        gr.Markdown("# MNIST Inpainting Demo with Arbitrary Masking")
 
         with gr.Row():
             image_selector = gr.Radio(
                 choices=["Image 1", "Image 2", "Image 3"],
-                label="Select Predefined CIFAR Image",
+                label="Select Predefined MNIST Image",
             )
             process_button = gr.Button("Run Inpainting", variant="primary")
 
         with gr.Row():
             upsampled_image_display = gr.Image(
-                label="Selected CIFAR Image",
+                label="Selected MNIST Image",
                 type="numpy",
                 image_mode="RGB",
                 interactive=False,
@@ -87,11 +133,11 @@ def demo():
 
         def load_selected_image(selected_option):
             if selected_option == "Image 1":
-                original_image = cifar_images[0]
+                original_image = mnist_images[0]
             elif selected_option == "Image 2":
-                original_image = cifar_images[1]
+                original_image = mnist_images[1]
             elif selected_option == "Image 3":
-                original_image = cifar_images[2]
+                original_image = mnist_images[2]
             else:
                 return None
 
@@ -103,16 +149,16 @@ def demo():
 
         def process_and_upsample(selected_option, mask_np):
             if selected_option == "Image 1":
-                original_image = cifar_images[0]
+                original_image = mnist_images[0]
             elif selected_option == "Image 2":
-                original_image = cifar_images[1]
+                original_image = mnist_images[1]
             elif selected_option == "Image 3":
-                original_image = cifar_images[2]
+                original_image = mnist_images[2]
             else:
                 return None
 
             downsampled_mask = downsample_mask(mask_np)
-            processed_image = inpaint_image(original_image, downsampled_mask)
+            processed_image = inpaint_image(original_image, downsampled_mask, model, 1000, 5)
             return upsample_image(processed_image)
 
         process_button.click(
